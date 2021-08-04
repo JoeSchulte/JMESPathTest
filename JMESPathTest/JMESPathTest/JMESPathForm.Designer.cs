11622df585c595c2babc3c0f50ec39d34fd0fde7
Joe Schulte <joe.schulte@daktronics.com>
2021-08-03 21:40:11 -0500

    App to test JMESPath via NuGet package.

---
+++ JMESPathTest/JMESPathTest/JMESPathForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace JMESPathTest
+{
+	partial class JMESPathForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxSource = new System.Windows.Forms.TextBox();
+			this.textBoxExpresion = new System.Windows.Forms.TextBox();
+			this.textBoxResult = new System.Windows.Forms.TextBox();
+			this.buttonExecute = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBoxSource
+			// 
+			this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSource.Location = new System.Drawing.Point(12, 12);
+			this.textBoxSource.MaxLength = 1000000;
+			this.textBoxSource.Multiline = true;
+			this.textBoxSource.Name = "textBoxSource";
+			this.textBoxSource.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxSource.Size = new System.Drawing.Size(776, 175);
+			this.textBoxSource.TabIndex = 0;
+			// 
+			// textBoxExpresion
+			// 
+			this.textBoxExpresion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxExpresion.Location = new System.Drawing.Point(12, 193);
+			this.textBoxExpresion.Name = "textBoxExpresion";
+			this.textBoxExpresion.Size = new System.Drawing.Size(695, 23);
+			this.textBoxExpresion.TabIndex = 1;
+			// 
+			// textBoxResult
+			// 
+			this.textBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxResult.Location = new System.Drawing.Point(12, 222);
+			this.textBoxResult.MaxLength = 1000000;
+			this.textBoxResult.Multiline = true;
+			this.textBoxResult.Name = "textBoxResult";
+			this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxResult.Size = new System.Drawing.Size(776, 153);
+			this.textBoxResult.TabIndex = 2;
+			// 
+			// buttonExecute
+			// 
+			this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonExecute.Location = new System.Drawing.Point(713, 192);
+			this.buttonExecute.Name = "buttonExecute";
+			this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+			this.buttonExecute.TabIndex = 3;
+			this.buttonExecute.Text = "Execute";
+			this.buttonExecute.UseVisualStyleBackColor = true;
+			this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+			// 
+			// JMESPathForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 387);
+			this.Controls.Add(this.buttonExecute);
+			this.Controls.Add(this.textBoxResult);
+			this.Controls.Add(this.textBoxExpresion);
+			this.Controls.Add(this.textBoxSource);
+			this.Name = "JMESPathForm";
+			this.Text = "JMESPath Tester";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JMESPathForm_FormClosing);
+			this.Load += new System.EventHandler(this.JMESPathForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxSource;
+		private System.Windows.Forms.TextBox textBoxExpresion;
+		private System.Windows.Forms.TextBox textBoxResult;
+		private System.Windows.Forms.Button buttonExecute;
+	}
+}
+
---
+++ JMESPathTest/JMESPathTest/JMESPathForm.cs
@@ -0,0 +1,86 @@
+﻿using DevLab.JmesPath;
+using JMESPathTest.Properties;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JMESPathTest
+{
+	public partial class JMESPathForm : Form
+	{
+		public JMESPathForm()
+		{
+			InitializeComponent();
+
+			if (Settings.Default.RequiresUpdate)
+			{
+				Settings.Default.Upgrade();
+				Settings.Default.RequiresUpdate = false;
+			}
+		}
+
+		private void JMESPathForm_Load(object sender, EventArgs e)
+		{
+			textBoxSource.Text = Settings.Default.Source;
+			textBoxExpresion.Text = Settings.Default.JMESPath;
+		}
+
+		private void JMESPathForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Settings.Default.Source = textBoxSource.Text;
+			Settings.Default.JMESPath = textBoxExpresion.Text;
+			Settings.Default.Save();
+		}
+
+		private void buttonExecute_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var jmes = new JmesPath();
+				var result = jmes.Transform(textBoxSource.Text, textBoxExpresion.Text);
+				textBoxResult.Text = format_json(result);
+			}
+			catch (Exception ex)
+			{
+				textBoxResult.Text = ex.Message;
+			}
+		}
+
+		public static string FormatJson(string json, string indent = "  ")
+		{
+			var indentation = 0;
+			var quoteCount = 0;
+			var escapeCount = 0;
+
+			var result =
+				from ch in json ?? string.Empty
+				let escaped = (ch == '\\' ? escapeCount++ : escapeCount > 0 ? escapeCount-- : escapeCount) > 0
+				let quotes = ch == '"' && !escaped ? quoteCount++ : quoteCount
+				let unquoted = quotes % 2 == 0
+				let colon = ch == ':' && unquoted ? ": " : null
+				let nospace = char.IsWhiteSpace(ch) && unquoted ? string.Empty : null
+				let lineBreak = ch == ',' && unquoted ? ch + Environment.NewLine + string.Concat(Enumerable.Repeat(indent, indentation)) : null
+				let openChar = (ch == '{' || ch == '[') && unquoted ? ch + Environment.NewLine + string.Concat(Enumerable.Repeat(indent, ++indentation)) : ch.ToString()
+				let closeChar = (ch == '}' || ch == ']') && unquoted ? Environment.NewLine + string.Concat(Enumerable.Repeat(indent, --indentation)) + ch : ch.ToString()
+				select colon ?? nospace ?? lineBreak ?? (
+					openChar.Length > 1 ? openChar : closeChar
+				);
+
+			return string.Concat(result);
+		}
+
+		private static string format_json(string json)
+		{
+			dynamic parsedJson = JsonConvert.DeserializeObject(json);
+			return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+		}
+	}
+}
